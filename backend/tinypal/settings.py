b65554d3c87a349d1452bae0442c65c51af37e2c
Keerthana P B <keerthanapb685@gmail.com>
2026-08-10 18:10:23 +0530

    Configure database for production

--- backend/tinypal/settings.py
+++ backend/tinypal/settings.py
@@ -100,10 +100,8 @@ WSGI_APPLICATION = 'tinypal.wsgi.application'
 
 DATABASES = {
     "default": dj_database_url.config(
-        default=(
-            f"sqlite:///"
-            f"{BASE_DIR / 'db.sqlite3'}"
-        )
+        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
+        conn_max_age=600,
     )
 }
 
