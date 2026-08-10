db8bbdc42701a5c06aa9f260aab4af9e762604b2
Keerthana P B <keerthanapb685@gmail.com>
2026-08-10 11:12:47 +0530

    Modified backend

--- backend/tinypal/settings.py
+++ backend/tinypal/settings.py
@@ -131,6 +131,14 @@ STATIC_URL = 'static/'
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
+    "http://127.0.0.1:5173",
+]
+
+CORS_ALLOW_CREDENTIALS = True
+
+CSRF_TRUSTED_ORIGINS = [
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
 ]
 
 REST_FRAMEWORK = {
